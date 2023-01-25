d5f9281667375aa32064903ec5ba7734d0dd6214
Federico Iglesias <igl.fede@gmail.com>
2023-01-25 20:07:59 +0100

    functionMoveUp

--- js/app.js
+++ js/app.js
@@ -14,15 +14,25 @@ document.getElementById('menuBtn').onclick = function () {
 }
 
 
-//percentage scroller bar and button up
+//percentage scroller bar and show button up
 let percentageScroller = () => {
     ubi = Math.floor(-((((foot.getBoundingClientRect().top)-665)/43.98)-101))
     bar.setAttribute('style', 'width: ' + ubi + '%');
     if( ubi >= 65 ){
-        console.log('holissssss')
         buttonUp.setAttribute('class','button__up')
     }else{buttonUp.setAttribute('class','button__up display_none')}
-}
-
+};
 
 window.addEventListener('scroll', percentageScroller);
+
+//function button up
+
+buttonUp.onclick = function() {
+    console.log(scrollY)
+    let moveTop = setInterval(function(){
+        window.scrollTo(0,scrollY - 4)
+        console.log(scrollY);
+        if(scrollY <= 0){clearInterval(moveTop)}}, 5)
+
+}
+
